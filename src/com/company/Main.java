f365bc3d2067d9f34c04c09aee13582b814b13bf
IYefremov <iyefremov@cyberiansoft.com>
2019-05-29 16:58:13 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static int a = 5;
+    public static int b = 10;
+
+    public static void main(String[] args) {
+
+        float result =   (float) a / b;
+
+        System.out.println(result);
+    }
+}
